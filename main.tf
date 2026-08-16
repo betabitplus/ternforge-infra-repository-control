ac48582568f8fad551906cd1f906e509fdc7b934
betabitplus <8123085+betabitplus@users.noreply.github.com>
2026-08-17 00:10:44 +0300

    fix: keep squash commit bodies blank

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "github_repository" "managed" {
   allow_merge_commit          = false
   allow_rebase_merge          = false
   squash_merge_commit_title   = "PR_TITLE"
-  squash_merge_commit_message = "PR_BODY"
+  squash_merge_commit_message = "BLANK"
 
   topics = each.value.topics
 
